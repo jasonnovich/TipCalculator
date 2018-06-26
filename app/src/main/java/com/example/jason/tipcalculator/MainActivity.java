12193dd86ef89685f759be4b7a5544f8a0102acd
Jason Novich <jasonnovich@gmail.com>
2018-06-26 10:45:31 +0300

    Extended version

--- app/src/main/java/com/example/jason/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/jason/tipcalculator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jason.tipcalculator;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int seekBarPercentage;
     private float enterBillFloat;
     private TextView totalBillTxTv;
+    //private String TAG = "TipCalculator";
+
+    //public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +37,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewSeekBar = findViewById(R.id.textViewSeekBarID);
         totalBillTxTv = findViewById(R.id.totalBillID);
 
+        //Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+        //Toast.makeTexT(getBaseContext(), "Hello", Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplication(), "Hello", Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+
+        //Log.v(TAG, MainActivity.this.getString(R.string.app_name));
+
         calculateButton.setOnClickListener(this);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%");
+                //textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%");
+                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + getString(R.string.percent));
+
+
             }
 
             @Override
@@ -57,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         calculate();
+        //TODO - Build Amazinbg app with this example
     }
 
     public void calculate(){
@@ -67,6 +85,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             result = enterBillFloat * seekBarPercentage/100;
             totalResultTextView.setText("Your tip will be: $" + String.valueOf((result)));
             totalBillTxTv.setText("Your Total to pay is: $" + String.valueOf(enterBillFloat + result));
+
+            //Log.v(TAG, String.valueOf(result));
 
         }else {
             Toast.makeText(MainActivity.this, "Please enter a bill amount!" , Toast.LENGTH_LONG).show();
